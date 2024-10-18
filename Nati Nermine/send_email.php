bb516d531a042536f61084baabdb86e6d41022e2
Nermine Nafti <115418226+nermine-nafti@users.noreply.github.com>
2024-10-18 23:07:23 +0100

    Add files via upload

---
+++ Nati Nermine/send_email.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstName = $_POST['firstName'];
+    $lastName = $_POST['lastName'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = 'nerminenafti.pro@gmail.com';
+    $subject = $subject;
+    $body = "First Name: $firstName\nLast Name: $lastName\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message could not be sent.";
+    }
+}
+?>
